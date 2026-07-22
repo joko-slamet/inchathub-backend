4e12d8f2083cb12a380b5a7b43d919dcb4e7e49a
Your Name <you@example.com>
2026-07-22 14:38:13 +0700

    change url

--- src/routes/webinarRegistration.route.ts
+++ src/routes/webinarRegistration.route.ts
@@ -1,11 +1,13 @@
 
 import { Router } from 'express';
+import { Role } from '../generated/prisma/enums';
 import { createWebinarRegistration, getWebinarRegistrations } from '../controllers/webinarRegistration.controller';
-import { authorize } from '../middlewares/auth';
+import { authenticate, authorize } from '../middlewares/auth';
+import { asyncHandler } from '../utils/asyncHandler';
 
 const router = Router();
 
-router.post('/', createWebinarRegistration);
-router.get('/', authorize('ADMIN'), getWebinarRegistrations);
+router.post('/', asyncHandler(createWebinarRegistration));
+router.get('/', authenticate, authorize(Role.ADMIN), asyncHandler(getWebinarRegistrations));
 
 export const webinarRegistrationRouter = router;
